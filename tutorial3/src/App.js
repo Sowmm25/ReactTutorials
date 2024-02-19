c3e8a07b4e6a80733268e261b620811c06bebfed
Sowmyashree M M <somm@BLR.ALLEGISINDIA.COM>
2024-02-19 12:05:59 +0530

    changes

--- tutorial3/src/App.js
+++ tutorial3/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  //let count=0;
+  const[count, setCount]=useState(0);
+  //console.log(count)
+  function handleAdd(){
+    // count+=1;
+    // console.log(count)
+    //setCount(count+1);
+    //setCount(count+1);
+    //even if call setcount 3 times value would be increased only once as it would be functioning as 0+1, 0+1, 0+1 because it would only call it in nxt render
+   
+   // if we need to count it 3 times we need to use functions like
+  //  setCount(count=>count+1);
+  //  setCount(count=>count+1);
+  //  setCount(count=>count+1);
+   
+    setCount(count+1);
+  }
+  function handleSub(){
+    setCount(count-1);
+  }
+  function handleReset(){
+    setCount(0);
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div className="box">
+        <p>{count}</p>
+        <button onClick={handleAdd} className="add">ADD</button>
+        {/* we can do the similar thing using anonymous function */}
+        {/* <button onClick={()=>setCount(count+1)} className="add2">ADD2</button> */}
+        <button onClick={handleSub} className="sub">SUB</button>
+         <button onClick={handleReset} className="reset">RESET</button>
+      </div>
 
-export default App;
+    </div>
+  )
+}
